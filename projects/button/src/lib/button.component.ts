7ac77609899c0bb57c30c2b8be26e4ec5e7319be
Swapnil Farande <swapnil.farande@arista.com>
2019-06-13 15:17:41 +0530

    feat: testing
    this is test to generate changelog

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'lib-button',
   template: `
     <p>
-      button works Demo!
+      button works Demo test!
     </p>
   `,
   styles: []
